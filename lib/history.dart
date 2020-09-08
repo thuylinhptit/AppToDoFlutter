f93c8a20453619ccfb42c0a0b3f425e1c1beef26
thuylinhptit <thuylinhptit1010@gmail.com>
2020-09-08 11:24:44 +0700

    commit 5

---
+++ lib/history.dart
@@ -0,0 +1,24 @@
+import 'package:apptodo_flutter/todo_task.dart';
+import 'package:apptodo_flutter/list_history.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class History extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'History', style: TextStyle( fontSize: 25, color: Colors.white, fontWeight: FontWeight.bold),
+        ),
+      ),
+//      body: Consumer<TodoTasks>(
+//        builder: (context, model, _ ){
+//          return ListHistory( listHistory: model.history,);
+//        },
+//      ),
+    );
+  }
+
+}
